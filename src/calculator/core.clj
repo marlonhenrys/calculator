1e2a11a2382ec8abd6389adeeac75012b2fd35c5
Marlon Henrique <silvamarlon87@gmail.com>
2022-03-12 19:36:59 -0300

    tests completed

--- src/calculator/core.clj
+++ src/calculator/core.clj
@@ -1,30 +1,45 @@
 (ns calculator.core
   (:gen-class))
 
-(defn c-sort [coll]
-  "Recursive quick sort implementation"
+(defn c-sort
+  "Ordenação por QuickSort recursivo"
   [coll]
-  (vec
-   (if (<= (count coll) 1)
-     coll
-     (let [pivot  (first coll)
-           others (rest coll)]
-       (concat
-        (c-sort (filter #(>= pivot %) others))
-        [pivot]
-        (c-sort (filter #(< pivot %) others)))))))
+  (if (<= (count coll) 1)
+    coll
+    (let [pivot  (first coll)
+          others (rest coll)]
+      (concat
+       (c-sort (filter #(>= pivot %) others))
+       [pivot]
+       (c-sort (filter #(< pivot %) others))))))
 
-(defn c-mean [coll]
-  (float (/ (reduce + coll) (count coll))))
+(defn c-mean
+  "Cálculo de média simples"
+  [coll]
+  (let [result (/ (reduce + coll) (count coll))]
+    (if (ratio? result)
+      (float result)
+      result)))
 
-(defn c-mode [coll]
-  (key (apply max-key val (frequencies coll))))
+(defn c-mode
+  "Cálculo de moda"
+  [coll]
+  (let [occurrences (frequencies coll)
+        max-occurrences (apply max (vals occurrences))]
+    (->> occurrences
+         (filter #(= max-occurrences (val %)))
+         (map key)
+         c-sort)))
 
-(defn c-range [coll]
+(defn c-range
+  "Cálculo de amplitude"
+  [coll]
   (let [sorted-coll (c-sort coll)]
     (- (last sorted-coll) (first sorted-coll))))
 
-(defn c-median [coll]
+(defn c-median
+  "Cálculo de mediana"
+  [coll]
   (let [sorted-coll (c-sort coll)
         coll-size   (count coll)
         middle      (quot coll-size 2)]
--- test/calculator/core_test.clj
+++ test/calculator/core_test.clj
@@ -3,28 +3,98 @@
             [calculator.core :refer :all]))
 
 (def empty-coll [])
-(def one-element-coll [])
-(def repetead-element-coll [])
-(def asc-coll [])
-(def desc-coll [])
-(def unsorted-coll [])
+(def one-element-coll [10])
+(def repeated-coll [20 20 20 20 20 20])
+(def asc-coll [1 2 3 4 5 6])
+(def desc-coll [5 4 3 2 1])
+(def unsorted-coll [6 3 4 2 5 1 7])
+(def repeated-elements-coll [1 3 4 5 3 2 4 4 3])
 
 (deftest c-sort-test
-  (testing "FIXME, I fail."
-    (is (= 0 (c-sort empty-coll)))))
+  (testing "Coleção vazia"
+    (is (= [] (c-sort empty-coll))))
+
+  (testing "Coleção com um único elemento"
+    (is (= one-element-coll (c-sort one-element-coll))))
+
+  (testing "Coleção com um único elemento repetidas vezes"
+    (is (= repeated-coll (c-sort repeated-coll))))
+
+  (testing "Coleção já ordenada de forma crescente"
+    (is (= asc-coll (c-sort asc-coll))))
+
+  (testing "Coleção já ordenada de forma decrescente"
+    (is (= [1 2 3 4 5] (c-sort desc-coll))))
+
+  (testing "Coleção não ordenada"
+    (is (= [1 2 3 4 5 6 7] (c-sort unsorted-coll))))
+
+  (testing "Coleção não ordenada com elementos repetidos"
+    (is (= [1 2 3 3 3 4 4 4 5] (c-sort repeated-elements-coll)))))
 
 (deftest c-mean-test
-  (testing "FIXME, I fail."
-    (is (= 2.5 (c-mean empty-coll)))))
+  (testing "Coleção vazia"
+    (is (thrown? Exception (c-mean empty-coll))))
+
+  (testing "Coleção com um único elemento"
+    (is (= 10 (c-mean one-element-coll))))
+
+  (testing "Coleção com um único elemento repetidas vezes"
+    (is (= 20 (c-mean repeated-coll))))
+
+  (testing "Coleção não ordenada"
+    (is (= 4 (c-mean unsorted-coll)))))
 
 (deftest c-mode-test
-  (testing "FIXME, I fail."
-    (is (= 0 (c-mode empty-coll)))))
+  (testing "Coleção vazia"
+    (is (thrown? Exception (c-mode empty-coll))))
+
+  (testing "Coleção com um único elemento"
+    (is (= [10] (c-mode one-element-coll))))
+
+  (testing "Coleção com um único elemento repetidas vezes"
+    (is (= [20] (c-mode repeated-coll))))
+
+  (testing "Coleção não ordenada sem elementos repetidos"
+    (is (= [1 2 3 4 5 6 7] (c-mode unsorted-coll))))
+
+  (testing "Coleção não ordenada com elementos repetidos"
+    (is (= [3 4] (c-mode repeated-elements-coll)))))
 
 (deftest c-range-test
-  (testing "FIXME, I fail."
-    (is (= 0 (c-range empty-coll)))))
+  (testing "Coleção vazia"
+    (is (thrown? Exception (c-range empty-coll))))
+
+  (testing "Coleção com um único elemento"
+    (is (= 0 (c-range one-element-coll))))
+
+  (testing "Coleção com um único elemento repetidas vezes"
+    (is (= 0 (c-range repeated-coll))))
+
+  (testing "Coleção já ordenada de forma crescente"
+    (is (= 5 (c-range asc-coll))))
+
+  (testing "Coleção já ordenada de forma decrescente"
+    (is (= 4 (c-range desc-coll))))
+
+  (testing "Coleção não ordenada"
+    (is (= 6 (c-range unsorted-coll)))))
 
 (deftest c-median-test
-  (testing "FIXME, I fail."
-    (is (= 0 (c-median empty-coll)))))
+  (testing "Coleção vazia"
+    (is (thrown? Exception (c-median empty-coll))))
+
+  (testing "Coleção com um único elemento"
+    (is (= 10 (c-median one-element-coll))))
+
+  (testing "Coleção com um único elemento repetidas vezes"
+    (is (= 20 (c-median repeated-coll))))
+
+  (testing "Coleção já ordenada de forma crescente"
+    (is (= 3.5 (c-median asc-coll))))
+
+  (testing "Coleção já ordenada de forma decrescente"
+    (is (= 3 (c-median desc-coll))))
+
+  (testing "Coleção não ordenada"
+    (is (= 4 (c-median unsorted-coll)))))
